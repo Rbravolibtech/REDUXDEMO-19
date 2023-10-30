375c2db0a742ce93d7417561eede1501b1cb0567
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-10-30 13:09:10 -0600

    did more react redux basics

--- redux-demo.js
+++ redux-demo.js
@@ -1,11 +1,19 @@
 const redux = require("redux");
 
 const counterReducer = (state = { counter: 0 }, action) => {
-	return {
-		counter: state.counter + 1,
-	};
-};
+	if (action.type === "increment") {
+		return {
+			counter: state.counter + 1,
+		};
+	}
 
+	if (action.type === "decrement") {
+		return {
+			counter: state.counter - 1,
+		};
+	}
+	return state;
+};
 const store = redux.createStore(counterReducer);
 
 const counterSubscriber = () => {
@@ -16,3 +24,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber);
 
 store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
